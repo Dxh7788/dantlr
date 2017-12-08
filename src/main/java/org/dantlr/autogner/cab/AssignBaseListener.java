c5f88d0061a25cb9375280f0dcf739b99287b589
xiaohong.dong <xiaohong.dong@htouhui.com>
2017-12-08 09:30:00 +0800

    new parser

---
+++ src/main/java/org/dantlr/autogner/cab/AssignBaseListener.java
@@ -0,0 +1,65 @@
+// Generated from E:/dtantlrlr/src/main/java/org/dantlr\Assign.g4 by ANTLR 4.7
+package org.dantlr.autogner.cab;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link AssignListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class AssignBaseListener implements AssignListener {
+	/**
+	 * Enter a parse tree produced by {@link AssignParser#assign}.
+	 *
+	 * @param ctx the parse tree
+	 */
+	public void enterAssign(AssignParser.AssignContext ctx) {
+
+	}
+
+	/**
+	 * Exit a parse tree produced by {@link AssignParser#assign}.
+	 *
+	 * @param ctx the parse tree
+	 */
+	public void exitAssign(AssignParser.AssignContext ctx) {
+
+	}
+
+	/**
+	 * Enter a parse tree produced by {@link AssignParser#expr}.
+	 *
+	 * @param ctx the parse tree
+	 */
+	public void enterExpr(AssignParser.ExprContext ctx) {
+
+	}
+
+	/**
+	 * Exit a parse tree produced by {@link AssignParser#expr}.
+	 *
+	 * @param ctx the parse tree
+	 */
+	public void exitExpr(AssignParser.ExprContext ctx) {
+
+	}
+
+	public void visitTerminal(TerminalNode node) {
+
+	}
+
+	public void visitErrorNode(ErrorNode node) {
+
+	}
+
+	public void enterEveryRule(ParserRuleContext ctx) {
+
+	}
+
+	public void exitEveryRule(ParserRuleContext ctx) {
+
+	}
+}
